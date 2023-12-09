bf00e2c249293982c807ab84852489babc03ff40
Ayushi <84962606+Ayushi3571@users.noreply.github.com>
2023-12-08 23:48:39 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,4 +1,5 @@
 module HaskellAssignment where
+
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
@@ -6,17 +7,17 @@ data Found = Match Int | NoMatch deriving Eq
 instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
-findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-findFirst returnVal = alsoFindFirst returnVal 0 
 
-alsoFindFirst :: Eq a => (a -> Bool) -> Int -> [a] -> Found
-alsoFindFirst _ _ [] = NoMatch
-alsoFindFirst needle index (x:xs)
-  | needle x = Match index
-  | otherwise = alsoFindFirst needle (index + 1) xs
+findFirst :: Eq a => (a -> Bool) -> [a] -> Found
+findFirst _ [] = NoMatch
+findFirst needle (x:xs)
+  | needle x = Match 0
+  | otherwise = case findFirst needle xs of
+                  NoMatch -> NoMatch
+                  Match index -> Match (index + 1)
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
-palindrome x = x == reverse x
+palindrome str = str == reverse str
